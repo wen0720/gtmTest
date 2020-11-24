1a0f1d80d12b0f84cd78a1308557a324dbeeb8e5
paper <paper.ws@medialand.tw>
2020-11-24 19:05:53 +0800

    gtm init

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'GTM 測試',
+      filename: 'index.html',
+      template: 'index.html',
+    })
+  ]
+};
